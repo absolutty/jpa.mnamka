d3f765038d00abd1869fe1300e3b002d5a1cddcf
Adam Hajro <ada.hajro@gmail.com>
2023-10-06 18:57:26 +0200

    Initial commit.

---
+++ src/main/java/sk/uniza/fri/mnamka/MnamkaApplication.java
@@ -0,0 +1,13 @@
+package sk.uniza.fri.mnamka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnamkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MnamkaApplication.class, args);
+	}
+
+}
