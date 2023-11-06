41af7093797b95a88af410876e79debe2bae7217
Adam Hajro <ada.hajro@gmail.com>
2023-11-06 19:22:39 +0100

    Remove of unused allergen.

--- src/main/java/sk/uniza/fri/mnamka/repository/AllergenRepository.java
+++
@@ -1,8 +0,0 @@
-package sk.uniza.fri.mnamka.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sk.uniza.fri.mnamka.model.AllergenModel;
-
-public interface AllergenRepository extends JpaRepository<AllergenModel, Integer> {
-
-}
