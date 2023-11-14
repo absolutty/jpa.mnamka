7772da531d4a528ac52737481c4700475f64f701
Adam Hajro <ada.hajro@gmail.com>
2023-11-14 01:28:26 +0100

    Resolving comments.

--- src/main/java/sk/uniza/fri/mnamka/exception/FoodException.java
+++ src/main/java/sk/uniza/fri/mnamka/exception/FoodException.java
@@ -4,7 +4,7 @@ public class FoodException {
 
     public static class IdNotFoundException extends RuntimeException {
         public IdNotFoundException(int id) {
-            super(String.format("The id of food you have provided is invalid. [%s]", id));
+            super(String.format("The id of food you have provided is invalid. [%d]", id));
         }
     }
 
