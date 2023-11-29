7d06cade0660646d23bf877097ab8e95680346c2
Adam Hajro <ada.hajro@gmail.com>
2023-11-29 11:08:42 +0100

    Edit foodtypes by admin.

--- src/main/java/sk/uniza/fri/mnamka/controller/pages/user/AdminController.java
+++ src/main/java/sk/uniza/fri/mnamka/controller/pages/user/AdminController.java
@@ -1,5 +1,6 @@
 package sk.uniza.fri.mnamka.controller.pages.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,8 @@ import sk.uniza.fri.mnamka.controller.PageController;
 import sk.uniza.fri.mnamka.exception.UserException;
 import sk.uniza.fri.mnamka.helper.Authenticator;
 import sk.uniza.fri.mnamka.helper.PathFormatter;
+import sk.uniza.fri.mnamka.service.FoodService;
+import sk.uniza.fri.mnamka.service.FoodTypeService;
 import sk.uniza.fri.mnamka.service.UserService;
 
 import java.util.List;
@@ -16,10 +19,24 @@ import java.util.List;
 @RequestMapping("/admin")
 public class AdminController extends PageController {
 
-    protected final UserService userService;
+    protected UserService userService;
+    protected FoodTypeService foodTypeService;
+    protected FoodService foodService;
 
     public AdminController(UserService userService) {
         this.userService = userService;
+    }
+
+    public AdminController(UserService userService, FoodTypeService foodTypeService) {
+        this.userService = userService;
+        this.foodTypeService = foodTypeService;
+    }
+
+    @Autowired
+    public AdminController(UserService userService, FoodTypeService foodTypeService, FoodService foodService) {
+        this.userService = userService;
+        this.foodTypeService = foodTypeService;
+        this.foodService = foodService;
     }
 
     @Override
@@ -30,8 +47,8 @@ public class AdminController extends PageController {
     @GetMapping
     public String getAdminPage(Model model) {
         if (Authenticator.isUserLoggedInAdmin()) {
-            List<String> usersIdentifiers = userService.getUsersIdentifiers();
-            model.addAttribute("listUserIdentifiers", usersIdentifiers);
+            model.addAttribute("listUserIdentifiers", userService.getUsersIdentifiers());
+            model.addAttribute("foodsTypesIdentifiers", foodTypeService.getFoodTypesIdentifiers());
 
             return getPathFormatter().getPageNameWithPath("admin_page");
         } else {
---
+++ src/main/java/sk/uniza/fri/mnamka/controller/pages/user/EditFoodTypeController.java
@@ -0,0 +1,79 @@
+package sk.uniza.fri.mnamka.controller.pages.user;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import sk.uniza.fri.mnamka.exception.UserException;
+import sk.uniza.fri.mnamka.helper.Authenticator;
+import sk.uniza.fri.mnamka.model.FoodTypeModel;
+import sk.uniza.fri.mnamka.service.FoodTypeService;
+import sk.uniza.fri.mnamka.service.UserService;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin/foodtypes")
+public class EditFoodTypeController extends AdminController {
+
+    public EditFoodTypeController(UserService userService, FoodTypeService foodTypeService) {
+        super(userService, foodTypeService);
+    }
+
+    @GetMapping("/edit")
+    public String showEditFoodTypeForm(@RequestParam("name") String name, Model model) {
+        if (Authenticator.isUserLoggedInAdmin()) {
+            List<String> usersIdentifiers = userService.getUsersIdentifiers();
+            model.addAttribute("listUserIdentifiers", usersIdentifiers);
+
+            FoodTypeModel foodTypeToBeEdited = foodTypeService.getFoodTypeByName(name);
+            model.addAttribute("foodTypeToBeEdited", foodTypeToBeEdited);
+
+            return getPathFormatter().getPageNameWithPath("admin_page");
+        } else {
+            throw new UserException.NotAllowedToAccess();
+        }
+    }
+    @PostMapping("/edit")
+    public String doEditUser(@ModelAttribute FoodTypeModel newFoodType, RedirectAttributes redirectAttributes) {
+        if (Authenticator.isUserLoggedInAdmin()) {
+            foodTypeService.updateExistingUser(newFoodType);
+            redirectAttributes.addFlashAttribute("success", "Kategória jedla uspešne upravená!");
+            return "redirect:/admin";
+        } else {
+            throw new UserException.NotAllowedToAccess();
+        }
+    }
+
+    @GetMapping("/delete")
+    public String showdeleteFoodTypeForm(@RequestParam("name") String name, Model model) {
+        if (Authenticator.isUserLoggedInAdmin()) {
+            List<String> usersIdentifiers = userService.getUsersIdentifiers();
+            model.addAttribute("listUserIdentifiers", usersIdentifiers);
+
+            FoodTypeModel foodTypeToBeDeleted = foodTypeService.getFoodTypeByName(name);
+            model.addAttribute("foodTypeToBeDeleted", foodTypeToBeDeleted);
+
+            return getPathFormatter().getPageNameWithPath("admin_page");
+        } else {
+            throw new UserException.NotAllowedToAccess();
+        }
+    }
+    @PostMapping("/delete")
+    public String doDeleteFoodType(@ModelAttribute FoodTypeModel toBeDeleted, RedirectAttributes redirectAttributes) {
+        if (Authenticator.isUserLoggedInAdmin()) {
+            try {
+                foodTypeService.deleteExistingFoodType(toBeDeleted);
+                redirectAttributes.addFlashAttribute("success", "Kategoria jedla uspesne vymazana!");
+            } catch (DataIntegrityViolationException e) {
+                redirectAttributes.addFlashAttribute("failure", e.getMessage());
+            }
+            return "redirect:/admin";
+
+        } else {
+            throw new UserException.NotAllowedToAccess();
+        }
+    }
+
+}
